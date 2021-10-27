ab1a94593f2ed453ed0b2af0d32a57f23e65a66f
kevin-weitgenant <kevin.weitgenant@gmail.com>
2021-10-26 22:49:26 -0300

    continua sendo um fracasso.
    trava dps do gerente ir tomar café

--- tentativa2.c
+++ tentativa2.c
@@ -3,17 +3,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdbool.h>
 
-const int LIMITE = 30;
+#define LIMITE 30
+#define CLIENTES_DIA 500
+
+
 
 int cadeiras_ocup = 0;
-int descansando =0;
+bool descansando = false;
 int tempo_atendimento;
+int nao_atendidos = 0;
 
 pthread_mutex_t cadeiras_mtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t descansando_mtx = PTHREAD_MUTEX_INITIALIZER;
 
-pthread_cond_t cond;
+pthread_cond_t cond_cafe;
 
 int entrada_clientes;
 
@@ -21,46 +26,50 @@ int entrada_clientes;
 
 void *entra_cliente(){
     while(1){
-        
-        if (descansando == 0){
-            //printf("\t\t\tantes de dar lock 1loop1!\n");
-            pthread_mutex_lock(&cadeiras_mtx);
-            entrada_clientes = rand()%4 ;
+        printf("entrou %d\n ", descansando);
+        pthread_mutex_lock(&cadeiras_mtx);
+        pthread_mutex_lock(&descansando_mtx);
+
+
+        if (descansando == false){
+            
+            
+            entrada_clientes = rand()%2;
             cadeiras_ocup+= entrada_clientes;
             
             if(cadeiras_ocup > LIMITE){
-                printf("Mais clientes do que o permitido. Cliente teve que se retirar!\n");
-                cadeiras_ocup-= entrada_clientes;
+                printf("Mais clientes do que o permitido. %d Clientes tiveram que se retirar!\n", cadeiras_ocup - LIMITE);
+                nao_atendidos += cadeiras_ocup - LIMITE;
+                cadeiras_ocup = LIMITE;
             }
-            if(entrada_clientes >= 0){
+            else if(entrada_clientes >= 0){
             printf("Entraram %d clientes.\nClientes na fila no momento = %d\n",entrada_clientes,cadeiras_ocup);
             }
-            pthread_mutex_unlock(&cadeiras_mtx);
+            
             sleep(1);
-            //printf("\t\t\tUNLOCK 1LOOP1\n");
+            
         }
 
-        else if (descansando == 1){
+        else if (descansando){
         
-            //printf("\t\t\tantes de dar lock 1loop2!\n");
-            pthread_mutex_lock(&cadeiras_mtx);
-            pthread_mutex_lock(&descansando_mtx);
+            
             printf("Cliente interrompe descanso do gerente e vai para fila\t %d\n",cadeiras_ocup);
-            descansando = 0;
-            pthread_cond_signal(&cond);
-            cadeiras_ocup+= 1;
+            descansando = false;
+            pthread_cond_signal(&cond_cafe);
+            cadeiras_ocup= 1;
 
 
-            pthread_mutex_unlock(&cadeiras_mtx); 
-            pthread_mutex_unlock(&descansando_mtx);
-            sleep(1);
-            //printf("\t\t\tUNLOCK 1LOOP2\n");
+
+            
+            
 
         }
 
     
 
-        //printf("\t\t\tentrou loop1!\n");
+        pthread_mutex_unlock(&cadeiras_mtx); 
+        pthread_mutex_unlock(&descansando_mtx);   
+        sleep(3);
 
     }
 }
@@ -68,19 +77,20 @@ void *entra_cliente(){
 void *gerente(){
     while(1){
         
-        while(cadeiras_ocup <= 0 && descansando == 1){
+        pthread_mutex_lock(&cadeiras_mtx);
+        pthread_mutex_lock(&descansando_mtx);
+
+
+        
+
+        while(descansando){
             printf("Gerente continua tomando seu cafézinho na tranquilidade!\n");
-            pthread_cond_wait(&cond, &descansando_mtx);
-
-
+            pthread_cond_wait(&cond_cafe, &descansando_mtx);
         }
         
         
         if (cadeiras_ocup > 0 ){
-            //printf("\t\t\tantes de dar lock 2loop1!\n");
-            pthread_mutex_lock(&cadeiras_mtx);
-            pthread_mutex_lock(&descansando_mtx);
-
+            
             cadeiras_ocup-= 1;
             tempo_atendimento = rand()%8 +5 ;
             //sleep(tempo_atendimento);
@@ -89,22 +99,23 @@ void *gerente(){
             
 
 
-            if( cadeiras_ocup<=0){
-                descansando = 1;
-                printf("Ninguém mais na fila, gerente vai tomar um cafézinho\t%d\n",cadeiras_ocup);
+            if( cadeiras_ocup==0){
+                descansando = true;
+                printf("Ninguém mais na fila, gerente vai tomar um cafézinho\t\n");
             }
-            pthread_mutex_unlock(&cadeiras_mtx);
-            pthread_mutex_unlock(&descansando_mtx);
-            //printf("\t\t\tUNLOCK 2LOOP1\n");
 
-            sleep(1);
+            
+
+            
         }
 
         
         
 
 
-        //printf("\t\t\tpassou loop2\n");
+        pthread_mutex_unlock(&cadeiras_mtx);
+        pthread_mutex_unlock(&descansando_mtx);
+        sleep(1);
     }
     
 }
@@ -117,7 +128,7 @@ int main(){
     pthread_mutex_init(&cadeiras_mtx, NULL);
     pthread_mutex_init(&descansando_mtx, NULL);
 
-    pthread_cond_init(&cond, NULL);
+    pthread_cond_init(&cond_cafe, NULL);
 
     pthread_create(&t_entra_cliente, NULL, &entra_cliente, NULL);
     pthread_create(&t_gerente, NULL, &gerente, NULL);
@@ -129,7 +140,7 @@ int main(){
 
     pthread_mutex_destroy(&cadeiras_mtx);
     pthread_mutex_destroy(&descansando_mtx);
-    pthread_cond_destroy(&cond);
+    pthread_cond_destroy(&cond_cafe);
     return 0;
     
 }
